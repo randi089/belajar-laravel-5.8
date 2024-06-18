75cfeca4d55185e899701d0634348582a1e8ca1e
randi089 <randifebriadi@gmail.com>
2024-06-18 20:54:07 +0700

    nambah beberapa fitur

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    $nama['nama'] = 'Tasya Aryati Sakinah';
+    return view('about', $nama);
 });
